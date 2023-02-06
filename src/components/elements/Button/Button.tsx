19c4d1adb1479fe2e5b923cc886f5004d4db4b89
rhosseinr <rhosseinr@outlook.com>
2023-02-06 23:15:22 +0330

    change button style

--- src/components/elements/Button/Button.tsx
+++ src/components/elements/Button/Button.tsx
@@ -8,11 +8,13 @@ const ButtonComponent = (props: ButtonProps, buttonRef) => {
     className = '',
     variant = 'primary',
     children,
-    active,
     width,
+    flat,
     loading = false,
     disabled = false,
+    size = 'md',
     style = {},
+    type = 'button',
     Component = 'button',
     ...rest
   } = props;
@@ -23,20 +25,34 @@ const ButtonComponent = (props: ButtonProps, buttonRef) => {
     styles.root,
     {
       [styles.primary]: variant === 'primary',
+      [styles.secondary]: variant === 'secondary',
+      [styles.dark]: variant === 'dark',
+      [styles.light]: variant === 'light',
       [styles.success]: variant === 'success',
       [styles.warning]: variant === 'warning',
       [styles.danger]: variant === 'danger',
+      [styles.error]: variant === 'error',
       [styles.info]: variant === 'info',
+    },
+    {
+      [styles.sm]: size === 'sm',
+      [styles.md]: size === 'md',
+      [styles.lg]: size === 'lg',
+      [styles.xl]: size === 'xl',
+    },
+    {
+      [styles.loading]: loading,
+      [styles.flat]: flat,
     },
     className
   );
 
   return (
     <Component
-      aria-pressed={active}
       data-variant={variant}
       className={rootClassName}
       disabled={disabled}
+      type={type}
       style={{
         width,
         ...style,
--- src/components/elements/Button/Button.types.ts
+++ src/components/elements/Button/Button.types.ts
@@ -4,19 +4,21 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   href?: string;
   className?: string;
   variant?:
-    | 'primary'
-    | 'secondary'
-    | 'danger'
-    | 'warning'
-    | 'success'
-    | 'info'
-    | 'light'
-    | 'dark';
-  active?: boolean;
+  | 'primary'
+  | 'secondary'
+  | 'danger'
+  | 'error'
+  | 'warning'
+  | 'success'
+  | 'info'
+  | 'light'
+  | 'dark';
   type?: 'submit' | 'reset' | 'button';
+  size?: 'sm' | 'md' | 'lg' | 'xl';
   Component?: string | JSXElementConstructor<any>;
   width?: string | number;
-  loading?: boolean;
-  disabled?: boolean;
+  loading: boolean;
+  disabled: boolean;
+  flat: boolean;
   ref?: any;
 }
